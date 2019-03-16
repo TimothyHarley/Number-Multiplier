3b72abf76b5eff63fd1d0070bb10c6c774062052
Tim Harley <Timothy.D.Harley@gmail.com>
2019-03-16 11:56:23 -0500

    numbers can be multiplied or squared

--- Number Multiplier/Program.cs
+++ Number Multiplier/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Number_Multiplier
 {
@@ -6,7 +8,41 @@ namespace Number_Multiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please type a list of numbers seprated by commas.");
+
+            string numberList = Console.ReadLine();
+ 
+            string[] numberSplit = numberList.Split(",");
+
+            Console.WriteLine("Would you like to multiply or square?");
+
+            string userOperator = Console.ReadLine();
+
+            if(userOperator == "multiply")
+            {
+                var control = 1;
+                foreach (string number in numberSplit)
+                {
+                    int numberfied = Int32.Parse(number);
+                    int solution = control * numberfied;
+                    control = solution;
+                }
+            Console.WriteLine($"Their product is: {control}");
+            }
+            else if(userOperator == "square")
+            {
+                var answer = new List<int>();
+                foreach (string number in numberSplit)
+                {
+                    int numberfied = Int32.Parse(number);
+                    int square = numberfied * numberfied;
+                    answer.Add(square);
+                }
+                Console.WriteLine($"This is each number squared: {string.Join(',', answer)}");
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
